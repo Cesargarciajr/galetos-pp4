7f6fab22da7699a04619956f99bd03993cbeca37
Cesargarciajr <>
2023-10-13 22:17:01 +0000

    fixed link urls

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var slider = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(slider, {
-        indicators: false,
-        height: 600
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var slider = document.querySelectorAll(".slider");
+  var instances = M.Slider.init(slider, {
+    indicators: false,
+    height: 600,
+  });
 });
