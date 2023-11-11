bf31d8a6b7039ed4198e930534c4270df6b5ef2d
Cesargarciajr <>
2023-11-11 10:10:24 +0000

    add database classes to create new order

--- galetosapp/admin.py
+++ galetosapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, NewOrder
 
-# Register your models here.
+
+@admin.register(NewOrder)
+class PostNewOrder(admin.ModelAdmin):
+    list_display = ('created_on', 'date', 'author', 'quantity', 'status')
+    list_filter = ('created_on', 'date', 'author', 'status')
+    search_fields = ('created_on', 'date', 'author',
+                     'first_name', 'last_name', 'phone_number', 'email')
+    actions = ['approve_orders']
+
+    def approve_orders(self, request, queryset):
+        queryset.update(status=True)
--- galetosapp/models.py
+++ galetosapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Pending"), (1, "Approved"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order_post")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return self.title
+        
+
+class NewOrder(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="new_order")
+    # Costumer Details
+    first_name = models.CharField(max_length=200, unique=False)
+    last_name = models.CharField(max_length=200, unique=False)
+    phone_number = models.CharField(max_length=200, unique=False)
+    email = models.CharField(max_length=200, unique=True)
+    quantity = models.IntegerField(unique=False)
+
+    # Order dates
+    date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.first_name
